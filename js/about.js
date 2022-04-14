41dc423fd33414550d4d6bc680703642c1141de5
ON Joseph <60672480+ONJoseph@users.noreply.github.com>
2022-04-14 22:44:55 +0100

    fix indentation

--- js/about.js
+++ js/about.js
@@ -2,11 +2,11 @@ const burger = document.querySelector('#hamburger');
 const closeMenu = document.querySelector('.close-menu');
 
 burger.addEventListener('click', () => {
-    document.querySelector('.nav-options').classList.toggle('active');
-    burger.classList.toggle('active');
+  document.querySelector('.nav-options').classList.toggle('active');
+  burger.classList.toggle('active');
 });
 
 closeMenu.addEventListener('click', () => {
-    document.querySelector('.nav-options').classList.toggle('active');
-    burger.classList.toggle('active');
+  document.querySelector('.nav-options').classList.toggle('active');
+  burger.classList.toggle('active');
 });
